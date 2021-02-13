5f107591e145eff50838642018a6fc3ac47a6003
jzeveleva <56284176+jzeveleva@users.noreply.github.com>
2021-02-13 00:43:11 -0500

    format changes

--- letterCount.go
+++ letterCount.go
@@ -16,32 +16,38 @@ func main() {
 	lca2 := findCount(str2)
 	lca3 := findCount(str3)
 
-	printArray(lca1)
-	printArray(lca2)
-	printArray(lca3)
+	printStorage(lca1)
+	printStorage(lca2)
+	printStorage(lca3)
 }
 
-// findCount will receive astring as a parameter and return an
-// return an array/slice of LetterCount structures/objects
+// findCount will receive a string as a parameter and
+// return a slice of LetterCount structures/objects
 
 func findCount(st string) []LetterCount {
-	lcArray := []LetterCount{}
+	letterCountStorage := []LetterCount{}
 	var i int
 	for i < len(st) {
 		tmp := st[i]
 		j := 1
 		for ; i+j < len(st) && tmp == st[i+j]; j++ {
 		}
-		lcArray = append(lcArray, LetterCount{Letter: st[i], Count: j})
+		letterCountStorage = append(letterCountStorage, LetterCount{Letter: st[i], Count: j})
 		i += j
 	}
-	return lcArray
+	return letterCountStorage
 }
 
-func printArray(arr []LetterCount) {
+// output collected information
+
+func printStorage(storage []LetterCount) {
 	fmt.Printf("[")
-	for _, val := range arr {
-		fmt.Printf("{%q:%d} ", val.Letter, val.Count)
+	for i, val := range storage {
+		if i == len(storage)-1 { // for the last element do not print ", "
+			fmt.Printf("{%q:%d}", val.Letter, val.Count)
+		} else {
+			fmt.Printf("{%q:%d}, ", val.Letter, val.Count)
+		}
 	}
 	fmt.Printf("]\n")
 }
